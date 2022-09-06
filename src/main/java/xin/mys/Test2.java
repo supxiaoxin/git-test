0ed70d3778ba4a39a935beb74ab7935294f8c5f8
xiaoxin <hx19461212@126.com>
2022-09-06 14:41:43 +0800

    hot-fix lonely version

--- src/main/java/xin/mys/Test2.java
+++ src/main/java/xin/mys/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2!");
         System.out.println("changes");
+        System.out.println("hot-fix changes lonely!");
     }
 }

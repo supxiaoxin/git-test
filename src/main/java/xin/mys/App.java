09f460464aef97572b812e554318800b1d79b5e8
xiaoxin <hx19461212@126.com>
2022-09-06 11:07:10 +0800

    IDEA pro version1

---
+++ src/main/java/xin/mys/App.java
@@ -0,0 +1,13 @@
+package xin.mys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello test git in IDEA!" );
+    }
+}
